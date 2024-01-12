c21da73c4735182183ca52d536f28b94959e8f3d
orun sunny <sunny.mukut@gmail.com>
2024-01-12 14:59:01 +0600

    added butoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello tic tac toe</h1>;
+  return (
+    <>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+      <button className="bg-white border border-grey-400 h-12 w-12 m-1 leading-9 text-lg">
+        x
+      </button>
+    </>
+  );
 }
